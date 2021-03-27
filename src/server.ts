3380ee8f268d0b07b8131fb13de55275945c5784
elton de campos <eltoncampos36@gmail.com>
2021-03-27 20:22:08 -0300

    [chore]: create server

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express, { request } from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (request, response) => {
+    return response.json({ message: "ok" });
+});
+
+app.post("/courses", (request, response) => {
+    const { name } = request.body;
+
+    return response.json({ name });
+});
+
+app.listen(3333, () => {
+    console.log("Server is running");
+});
