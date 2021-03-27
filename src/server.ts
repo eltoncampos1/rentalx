3a5d6f94d75c61b721e9b11175a81199e4b6ed3e
elton de campos <eltoncampos36@gmail.com>
2021-03-27 20:41:12 -0300

    [chore]: create category routes

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,10 @@
-import express, { request } from "express";
+import express from "express";
+
+import routes from "./routes";
 
 const app = express();
 app.use(express.json());
-
-app.get("/", (request, response) => {
-    return response.json({ message: "ok" });
-});
-
-app.post("/courses", (request, response) => {
-    const { name } = request.body;
-
-    return response.json({ name });
-});
+app.use(routes);
 
 app.listen(3333, () => {
     console.log("Server is running");
