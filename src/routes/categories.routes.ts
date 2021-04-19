30dd87e59edfc04d706fcd440c9b70b1e5ca4c42
elton de campos <eltoncampos36@gmail.com>
2021-04-19 10:56:06 -0300

    chore: create function to return all categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -13,4 +13,10 @@ categoriesRoutes.post("/", (request, response) => {
   return response.status(201).send();
 });
 
+categoriesRoutes.get("/", (request, response) => {
+  const listAll = categoriesRepository.list();
+
+  return response.json(listAll);
+});
+
 export { categoriesRoutes };
